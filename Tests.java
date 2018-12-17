415622aec901de2fc5e552637b0800ed20330f5b
Brent <brenteliason@gmail.com>
2018-12-17 12:05:01 -0500

    Passes first two tests, checks for input file, loads contents into WordSearch object

--- Tests.java
+++ Tests.java
@@ -3,36 +3,41 @@ package kata;
 public class Tests {
 
 	public static void main(String[] args) {
-		System.out.println("Beginning tests for word search kata");
+		System.out.println("Beginning tests for word search kata...\n");
 
 
 
 		//test that input included on command line
 		System.out.println("Checking for input file on command line...");
 		try {
-			if (args[0] != null)
-				System.out.println("\tPAsSED...Input file on command line");
+			if (args[0] != null) {
+				System.out.println("\tPASSED...Input file on command line\n");
+
+				//test that input was loaded into WordSearch object
+				System.out.println("Checking that input was properly loaded");
+				WordSearch example = new WordSearch(args[0]);
+				if (example.getWordSearchContents() != null) {
+					System.out.println("\tPASSED...word search contents loaded\n");
+				}
+				else
+					System.out.println("\tFAILED...word search has no contents\n");
+			}
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("\tFAILED...No input file on command line");
+			System.out.println("\tFAILED...No input file on command line\n");
 		}
 
-
-		//test that input was loaded into WordSearch object
-		System.out.println("Checking that input was properly loaded");
-		WordSearch example = new WordSearch(args[0]);
-
 		//test that input is properly formatted
-		System.out.println("Checking that input is in acceptable format");
+		System.out.println("Checking that input is in acceptable format...");
 
 		//check for output
-		System.out.println("Checking that output is present");
+		System.out.println("Checking that output is present...");
 
 		//check formatting of output
-		System.out.println("Checking format of output");
+		System.out.println("Checking format of output...");
 
 		//check output results with expected results
-		System.out.println("Comparing output with expected results");
+		System.out.println("Comparing output with expected results...");
 
 	}
 
--- WordSearch.java
+++ WordSearch.java
@@ -1,15 +1,43 @@
 package kata;
 
+import java.util.Scanner;
+import java.io.*;
 
 public class WordSearch {
-	String input;
+	String inputFileContents = null;
+	//String wordSearchFile;
+	//need variable to store contents of word search, 2d array?
 
 	WordSearch(String file) {
-		System.out.println("Created WordSearch object w following input: ");
-		System.out.println(file);
-		input = file;
+		//System.out.println("Created WordSearch object w following input: ");
+		//System.out.println(file);
+		//wordSearchFile = file;
+		loadWordSearch(file);
 	}
 
+	public boolean loadWordSearch(String file) {
+		try {
+			Scanner scan = new Scanner(new File(file));
+			int lineCount = 0;
+			inputFileContents = "";
+			while (scan.hasNextLine() == true) {
+				String temp = "";
+				temp = scan.nextLine();
+				inputFileContents += "\n" + temp;
+				//System.out.println(lineCount + ": " + temp);
+				lineCount++;
+			}
+			return true;
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			return false;
+		}
+	}
 
+	public String getWordSearchContents() {
+		System.out.println("\tgetWordSearchContents method called, contents are as follows:\n" + inputFileContents);
+		return inputFileContents;
+	}
 
 }
