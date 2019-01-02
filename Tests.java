d1898af67d8a479db86093576604e3cf17904cfc
Brent <brenteliason@gmail.com>
2019-01-02 08:26:35 -0500

    Cleaned up code in Tests, added clarifying comments

--- Tests.java
+++ Tests.java
@@ -7,30 +7,12 @@ public class Tests {
 
 		String wordSearchFile = null;
 		WordSearch example = new WordSearch();
-		//test that input included on command line
+
 		System.out.println("Checking for input file on command line...");
 		try {
 			if (args[0] != null) {
 				System.out.println("\tPASSED...Input file on command line\n");
 				wordSearchFile = args[0];
-				//test that input was loaded into WordSearch object
-				System.out.println("Checking that input is properly formatted");
-				boolean outcome = example.loadWordSearch(args[0]);
-				if (outcome == true)
-					System.out.println("\tPASSED...word search file is properly formatted\n");
-				else {
-					System.out.println("\tFAILED...word search file was not properly formatted\n");
-					return;
-				}
-
-				System.out.println("Checking that input was properly loaded");
-				if (example.getWordSearchContents() != null) {
-					System.out.println("\tPASSED...word search contents loaded\n");
-				}
-				else {
-					System.out.println("\tFAILED...word search has no contents\n");
-					return;
-				}
 			}
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
@@ -38,7 +20,24 @@ public class Tests {
 			return;
 		}
 
-		//test that first line of input file has been converted to array of Strings
+		System.out.println("Checking that input is properly formatted...");
+		boolean outcome = example.loadWordSearch(args[0]);
+		if (outcome == true)
+			System.out.println("\tPASSED...word search file is properly formatted\n");
+		else {
+			System.out.println("\tFAILED...word search file was not properly formatted\n");
+			return;
+		}
+
+		System.out.println("Checking that input was properly loaded...");
+		if (example.getWordSearchContents() != null) {
+			System.out.println("\tPASSED...word search contents loaded\n");
+		}
+		else {
+			System.out.println("\tFAILED...word search has no contents\n");
+			return;
+		}
+
 		System.out.println("Checking for list of words to search for...");
 		if (example.getSearchWords() == null) {
 			System.out.println("\tFAILED...no search words\n");
@@ -55,27 +54,24 @@ public class Tests {
 		else
 			System.out.println("\tPASSED...word search grid found\n");
 
-		//check for output
-		System.out.println("Checking that output is present...");
+		System.out.println("Checking if all words were found in word search...");
 		example.findWordsInGrid();
-		if (example.hasAllAnswers()) {
+		if (example.hasAllAnswers())
 			System.out.println("\tPASSED...all words found in word search\n");
-		}
 		else {
-			System.out.println("\tFAILED...not all words were found in word search\n");
-			return;
+			System.out.println("\tFAILED...not all words were found in word search (non-fatal error)\n");
 		}
 
-		//check output results with expected results
-		System.out.println("Checking answers with expected results...");
-
+		System.out.println("Comparing answers with expected results (only works with known files)...");
 		String [] wordPositions = example.getSolutions();
 		String [] expected = new String[wordPositions.length];
+		boolean knownFile = false;
 
 		if (wordSearchFile.equals("kata/input1.txt")) {
 			expected[0] = "(0,1),(1,1),(2,1)";
 			expected[1] = "(0,0),(1,0),(2,0)";
 			expected[2] = "(0,2),(1,2),(2,2)";
+			knownFile = true;
 		}
 		if (wordSearchFile.equals("kata/input2.txt")) {
 			expected[0] = "(0,6),(0,7),(0,8),(0,9),(0,10)";
@@ -85,21 +81,26 @@ public class Tests {
 			expected[4] = "(2,1),(3,2),(4,3),(5,4),(6,5)";
 			expected[5] = "(3,3),(2,2),(1,1),(0,0)";
 			expected[6] = "(4,0),(3,1),(2,2),(1,3),(0,4)";
+			knownFile = true;
 		}
 
-		boolean wordsFoundAsExpected = true;
-		for (int i = 0; i < wordPositions.length; i++) {
-			if (expected[i].equals(wordPositions[i]) != true) {
-				System.out.println("\tFAILED...word " + i + " not found in correct position\n");
-				wordsFoundAsExpected = false;
+		if (knownFile == true) {
+			boolean wordsFoundAsExpected = true;
+			for (int i = 0; i < wordPositions.length; i++) {
+				if (expected[i].equals(wordPositions[i]) != true) {
+					System.out.println("\tFAILED...word " + i + " not found in correct position\n");
+					wordsFoundAsExpected = false;
+				}
 			}
+			if (wordsFoundAsExpected == true)
+				System.out.println("\tPASSED...all words found in correct position\n");
+			else
+				return;
 		}
-		if (wordsFoundAsExpected == true)
-			System.out.println("\tPASSED...all words found in correct position\n");
-		else
-			return;
+		else {
+				System.out.println("\tN/A...unknown file, no expected answers to compare with\n");
+		}
 
-		//TESTING OVER - FINAL OUTPUT OF PROGRAM
 		System.out.println("ALL TESTS COMPLETE...now printing final output");
 		System.out.println("************************************************\n");
 		String answers = example.getAnswers();
